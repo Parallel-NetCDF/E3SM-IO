9b9d4d66228c363fcec15104ea3ad0473cce2889
khou <khou2020@outlook.com>
2021-06-21 15:45:48 -0500

    Support subfiling: Add command line option to set number of subfiles; Set the corresponding subfiling property if the driver supports it;

--- src/drivers/e3sm_io_driver_adios2.cpp
+++ src/drivers/e3sm_io_driver_adios2.cpp
@@ -128,6 +128,9 @@ int e3sm_io_driver_adios2::create (std::string path, MPI_Comm comm, MPI_Info inf
     aerr = adios2_set_engine (fp->iop, "BP4");
     CHECK_AERR
 
+    sprintf (ng, "%d", cfg->num_group);
+    aerr = adios2_set_parameter (fp->iop, "substreams", ng);
+    CHECK_AERR
     aerr = adios2_set_parameter (fp->iop, "CollectiveMetadata", "OFF");
     CHECK_AERR
     fp->ep   = NULL;
