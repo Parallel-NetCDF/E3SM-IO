297fde555f91cf40f6176aae1c1dfef6c5cdb54d
wkliao <wkliao@users.noreply.github.com>
2021-07-18 17:24:23 -0500

    Move function declarations
    move e3sm_io_case_F.hpp and e3sm_io_case_G.hpp into e3sm_io_case.hpp

--- src/cases/e3sm_io_case.hpp
+++ src/cases/e3sm_io_case.hpp
@@ -8,10 +8,11 @@
  *********************************************************************/
 //
 #pragma once
-//
-#include <e3sm_io.h>
 
+#include <vector>
+#include <e3sm_io.h>
 #include <e3sm_io_driver.hpp>
+
 class e3sm_io_case {
    public:
     virtual ~e3sm_io_case () {};
@@ -64,3 +65,145 @@ class e3sm_io_case_G_scorpio : public e3sm_io_case_G {
     int rd_test (e3sm_io_config &cfg, e3sm_io_decom &decom, e3sm_io_driver &driver);
 };
 #endif
+
+/*---- functions for F case -------------------------------------------------*/
+extern int
+def_F_case_h0(e3sm_io_driver &driver,
+              int ncid,
+              const MPI_Offset dims[2],
+              int nvars,
+              int *varids);
+
+extern int
+inq_F_case_h0(e3sm_io_driver &driver,
+              int ncid,           /* file ID */
+              MPI_Offset dims[2], /* dimension sizes */
+              int nvars,          /* number of variables */
+              int *varids);       /* variable IDs */
+
+extern int
+def_F_case_h1(e3sm_io_driver &driver,
+              int ncid,
+              const MPI_Offset dims[2],
+              int nvars,
+              int *varids);
+
+extern int
+inq_F_case_h1(e3sm_io_driver &driver,
+              int ncid,           /* file ID */
+              MPI_Offset dims[2], /* dimension sizes */
+              int nvars,          /* number of variables */
+              int *varids);       /* variable IDs */
+
+extern int
+run_vard_F_case(e3sm_io_config &cfg,
+                e3sm_io_decom &decom,
+                e3sm_io_driver &driver,
+                double *dbl_bufp,    /* buffer for fixed size double var */
+                itype *rec_bufp,     /* buffer for rec floating point var */
+                char *txt_buf,       /* buffer for char var */
+                int *int_buf);       /* request's block lengths */
+
+extern int
+run_varn_F_case(e3sm_io_config &cfg,
+                e3sm_io_decom &decom,
+                e3sm_io_driver &driver);
+
+extern int
+run_varn_F_case_rd(e3sm_io_config &cfg,
+                   e3sm_io_decom &decom,
+                   e3sm_io_driver &driver,
+                   double **dbl_bufp,   /* buffer for fixed size double var */
+                   itype **rec_bufp,    /* buffer for rec floating point var */
+                   char *txt_buf,       /* buffer for char var */
+                   int *int_buf);       /* buffer for int var */
+
+extern int
+def_F_case_h0(e3sm_io_config &cfg,
+              e3sm_io_decom  &decom,
+              e3sm_io_driver &driver,
+              int ncid,
+              int *varids);
+
+extern int
+def_F_case_h1(e3sm_io_config &cfg,
+              e3sm_io_decom  &decom,
+              e3sm_io_driver &driver,
+              int ncid,
+              int *varids);
+
+extern int
+blob_F_case(e3sm_io_config &cfg,
+            e3sm_io_decom &decom,
+            e3sm_io_driver &driver);
+
+
+/*---- functions for G case -------------------------------------------------*/
+extern int
+def_G_case_h0(e3sm_io_driver &driver,
+              int ncid,                    /* file ID */
+              const MPI_Offset dims_D1[1], /* dimension sizes of decomposition 1 */
+              const MPI_Offset dims_D2[1], /* dimension sizes of decomposition 2 */
+              const MPI_Offset dims_D3[2], /* dimension sizes of decomposition 3 */
+              const MPI_Offset dims_D4[2], /* dimension sizes of decomposition 4 */
+              const MPI_Offset dims_D5[2], /* dimension sizes of decomposition 5 */
+              const MPI_Offset dims_D6[2], /* dimension sizes of decomposition 6 */
+              int nvars,                   /* number of variables */
+              int *varids);                /* variable IDs */
+
+extern int
+inq_G_case_h0(e3sm_io_driver &driver,
+              int ncid,              /* file ID */
+              MPI_Offset dims_D1[1], /* dimension sizes of decomposition 1 */
+              MPI_Offset dims_D2[1], /* dimension sizes of decomposition 2 */
+              MPI_Offset dims_D3[2], /* dimension sizes of decomposition 3 */
+              MPI_Offset dims_D4[2], /* dimension sizes of decomposition 4 */
+              MPI_Offset dims_D5[2], /* dimension sizes of decomposition 5 */
+              MPI_Offset dims_D6[2], /* dimension sizes of decomposition 6 */
+              int nvars,             /* number of variables */
+              int *varids);          /* variable IDs */
+
+extern int
+run_varn_G_case(e3sm_io_config &cfg,
+                e3sm_io_decom &decom,
+                e3sm_io_driver &driver,
+                int *D1_fix_int_bufp,     /* D1 fix int buffer */
+                int *D2_fix_int_bufp,     /* D2 fix int buffer */
+                int *D3_fix_int_bufp,     /* D3 fix int buffer */
+                int *D4_fix_int_bufp,     /* D4 fix int buffer */
+                int *D5_fix_int_bufp,     /* D5 fix int buffer */
+                double *D1_rec_dbl_bufp,  /* D1 rec double buffer */
+                double *D3_rec_dbl_bufp,  /* D3 rec double buffer */
+                double *D4_rec_dbl_bufp,  /* D4 rec double buffer */
+                double *D5_rec_dbl_bufp,  /* D5 rec double buffer */
+                double *D6_rec_dbl_bufp,  /* D6 rec double buffer */
+                double *D1_fix_dbl_bufp); /* D1 fix double buffer */
+
+extern int
+run_varn_G_case_rd(e3sm_io_config &cfg,
+                   e3sm_io_decom &decom,
+                   e3sm_io_driver &driver,
+                   int **D1_fix_int_bufp,     /* D1 fix int buffer */
+                   int **D2_fix_int_bufp,     /* D2 fix int buffer */
+                   int **D3_fix_int_bufp,     /* D3 fix int buffer */
+                   int **D4_fix_int_bufp,     /* D4 fix int buffer */
+                   int **D5_fix_int_bufp,     /* D5 fix int buffer */
+                   double **D1_rec_dbl_bufp,  /* D1 rec double buffer */
+                   double **D3_rec_dbl_bufp,  /* D3 rec double buffer */
+                   double **D4_rec_dbl_bufp,  /* D4 rec double buffer */
+                   double **D5_rec_dbl_bufp,  /* D5 rec double buffer */
+                   double **D6_rec_dbl_bufp,  /* D6 rec double buffer */
+                   double **D1_fix_dbl_bufp); /* D1 fix double buffer */
+
+extern int
+blob_G_case(e3sm_io_config &cfg,
+            e3sm_io_decom &decom,
+            e3sm_io_driver &driver);
+
+extern int
+def_G_case(e3sm_io_config &cfg,
+           e3sm_io_decom  &decom,
+           e3sm_io_driver &driver,
+           int ncid,
+           int *varids);
+
