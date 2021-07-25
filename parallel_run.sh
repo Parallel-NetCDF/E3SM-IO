7a43bd095b67e898c1c033a3c12620d6887356e1
wkliao <wkliao@users.noreply.github.com>
2021-07-25 15:10:21 -0500

    add env variable for mpiexec command

--- parallel_run.sh
+++ parallel_run.sh
@@ -14,36 +14,36 @@ EXEC=src/e3sm_io
 export LD_LIBRARY_PATH=${HDF5_LIB_PATH}:${LD_LIBRARY_PATH}
 
 # F case
-mpiexec -n 16 ${EXEC} ${F_DECOMP} -o blob_F_out.nc -x blob -a pnetcdf      -r 25
-mpiexec -n 16 ${EXEC} ${F_DECOMP} -o can_F_out.nc  -x canonical -a pnetcdf -r 25
+${TESTMPIRUN} -n 16 ${EXEC} ${F_DECOMP} -o blob_F_out.nc -x blob -a pnetcdf      -r 25
+${TESTMPIRUN} -n 16 ${EXEC} ${F_DECOMP} -o can_F_out.nc  -x canonical -a pnetcdf -r 25
 
 if test "x${ENABLE_HDF5}" = x1 ; then
-   mpiexec -n 16 ${EXEC} ${F_DECOMP} -o blob_F_out.h5 -x blob      -a hdf5       -r 25
+   ${TESTMPIRUN} -n 16 ${EXEC} ${F_DECOMP} -o blob_F_out.h5 -x blob      -a hdf5       -r 25
 fi
 if test "x${HDF5_HAVE_DWRITE_MULTI}" = x1 ; then
-   mpiexec -n 16 ${EXEC} ${F_DECOMP} -o can_F_out.h5  -x canonical -a hdf5_md    -r 25
+   ${TESTMPIRUN} -n 16 ${EXEC} ${F_DECOMP} -o can_F_out.h5  -x canonical -a hdf5_md    -r 25
 fi
 if test "x${ENABLE_LOGVOL}" = x1 ; then
-   mpiexec -n 16 ${EXEC} ${F_DECOMP} -o log_F_out.h5  -x log       -a hdf5_log   -r 25
+   ${TESTMPIRUN} -n 16 ${EXEC} ${F_DECOMP} -o log_F_out.h5  -x log       -a hdf5_log   -r 25
 fi
 if test "x${ENABLE_ADIOS2}" = x1 ; then
-   mpiexec -n 16 ${EXEC} ${F_DECOMP} -o blob_F_out    -x blob      -a adios -g 2 -r 25
+   ${TESTMPIRUN} -n 16 ${EXEC} ${F_DECOMP} -o blob_F_out    -x blob      -a adios -g 2 -r 25
 fi
 
 # G case
-mpiexec -n 16 ${EXEC} ${G_DECOMP} -o blob_G_out.nc -x blob -a pnetcdf
-mpiexec -n 16 ${EXEC} ${G_DECOMP} -o can_G_out.nc
+${TESTMPIRUN} -n 16 ${EXEC} ${G_DECOMP} -o blob_G_out.nc -x blob -a pnetcdf
+${TESTMPIRUN} -n 16 ${EXEC} ${G_DECOMP} -o can_G_out.nc
 
 if test "x${ENABLE_HDF5}" = x1 ; then
-   mpiexec -n 16 ${EXEC} ${G_DECOMP} -o blob_G_out.h5 -x blob      -a hdf5
+   ${TESTMPIRUN} -n 16 ${EXEC} ${G_DECOMP} -o blob_G_out.h5 -x blob      -a hdf5
 fi
 if test "x${HDF5_HAVE_DWRITE_MULTI}" = x1 ; then
-   mpiexec -n 16 ${EXEC} ${G_DECOMP} -o can_G_out.h5  -x canonical -a hdf5_md
+   ${TESTMPIRUN} -n 16 ${EXEC} ${G_DECOMP} -o can_G_out.h5  -x canonical -a hdf5_md
 fi
 if test "x${ENABLE_LOGVOL}" = x1 ; then
-   mpiexec -n 16 ${EXEC} ${G_DECOMP} -o log_G_out.h5  -x log       -a hdf5_log
+   ${TESTMPIRUN} -n 16 ${EXEC} ${G_DECOMP} -o log_G_out.h5  -x log       -a hdf5_log
 fi
 if test "x${ENABLE_ADIOS2}" = x1 ; then
-   mpiexec -n 16 ${EXEC} ${G_DECOMP} -o blob_G_out    -x blob      -a adios -g 2
+   ${TESTMPIRUN} -n 16 ${EXEC} ${G_DECOMP} -o blob_G_out    -x blob      -a adios -g 2
 fi
 
