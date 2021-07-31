52fbcb5b4297938cb772dd3ff86c3b80b6434460
wkliao <wkliao@users.noreply.github.com>
2021-07-31 14:15:21 -0500

    move file name construction to wr_test

--- src/cases/e3sm_io_case_F.cpp
+++ src/cases/e3sm_io_case_F.cpp
@@ -9,10 +9,12 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
-//
-#include <assert.h>
+
+#include <stdio.h>
 #include <stdlib.h>
-//
+#include <string.h>
+#include <assert.h>
+
 #include <e3sm_io.h>
 #include <e3sm_io_err.h>
 #include <e3sm_io_case.hpp>
@@ -27,10 +29,11 @@ e3sm_io_case_F::~e3sm_io_case_F () {
 }
 
 int e3sm_io_case_F::wr_test(e3sm_io_config &cfg,
-                            e3sm_io_decom &decom,
+                            e3sm_io_decom  &decom,
                             e3sm_io_driver &driver)
 {
     int err=0, nvars, nrecs;
+    char base_name[1040], outfile[1056], *ext;
 
     nvars = cfg.nvars;
     nrecs = cfg.nrecs;
@@ -39,18 +42,54 @@ int e3sm_io_case_F::wr_test(e3sm_io_config &cfg,
     err = calc_metadata(&cfg, &decom);
     CHECK_ERR
 
+    ext = strrchr(cfg.out_path, '.');
+
     if (cfg.hx == 0 || cfg.hx == -1) {  /* h0 file */
+        /* construct file name */
+        if (ext == NULL || (strcmp(ext, ".nc") && strcmp(ext, ".h5"))) {
+            sprintf(base_name, "%s_h0", cfg.out_path);
+        }
+        else {
+            sprintf(base_name, "%s", cfg.out_path);
+            sprintf(base_name + (ext - cfg.out_path), "_h0%s", ext);
+        }
+        if (cfg.strategy == blob) /* set output subfile name */
+            sprintf(outfile, "%s.%04d", base_name, cfg.subfile_ID);
+        else
+            strcpy(outfile, base_name);
+
+        cfg.hist  = h0;
         cfg.nvars = 414;
         cfg.nrecs = 1;
-        err = var_wr_F_case(cfg, decom, driver);
+        err = var_wr_F_case(cfg, decom, driver, outfile);
         CHECK_ERR
+
+        /* report timing breakdowns */
+        report_timing_WR(&cfg, &driver, &decom, base_name);
     }
 
     if (cfg.hx == 1 || cfg.hx == -1) {  /* h1 file */
+        /* construct file name */
+        if (ext == NULL || (strcmp(ext, ".nc") && strcmp(ext, ".h5"))) {
+            sprintf(base_name, "%s_h1", cfg.out_path);
+        }
+        else {
+            sprintf(base_name, "%s", cfg.out_path);
+            sprintf(base_name + (ext - cfg.out_path), "_h1%s", ext);
+        }
+        if (cfg.strategy == blob) /* set output subfile name */
+            sprintf(outfile, "%s.%04d", base_name, cfg.subfile_ID);
+        else
+            strcpy(outfile, base_name);
+
+        cfg.hist  = h1;
         cfg.nvars = 51;
         cfg.nrecs = nrecs;
-        err = var_wr_F_case(cfg, decom, driver);
+        err = var_wr_F_case(cfg, decom, driver, outfile);
         CHECK_ERR
+
+        /* report timing breakdowns */
+        report_timing_WR(&cfg, &driver, &decom, base_name);
     }
 
     cfg.nvars = nvars;
