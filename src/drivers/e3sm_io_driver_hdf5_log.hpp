4c198a739e9dc4422595444283c66a00b76b7dc1
khou <khou2020@outlook.com>
2021-09-11 00:12:49 -0500

    HDF5 log driver inherit HDF5 driver: Reuse member functions other than dataset write and file open/create

--- src/drivers/e3sm_io_driver_hdf5_log.hpp
+++ src/drivers/e3sm_io_driver_hdf5_log.hpp
@@ -19,72 +19,26 @@
 
 #include <e3sm_io_driver.hpp>
 
-class e3sm_io_driver_hdf5_log : public e3sm_io_driver {
-    class hdf5_file_log {
-       public:
-        e3sm_io_driver_hdf5_log &driver;
-        e3sm_io_config *cfg;
-        hid_t id;
-        std::vector<hid_t> dids;
-        std::vector<hsize_t> dsizes;
-        MPI_Offset recsize = 0;
-        MPI_Offset putsize = 0;
-        MPI_Offset getsize = 0;
-        int rank;
-
-        hdf5_file_log (e3sm_io_driver_hdf5_log &x) : driver (x), cfg{x.cfg} {};
-    };
-
-    std::vector<hdf5_file_log *> files;
-    hid_t dxplid_coll;
-    hid_t dxplid_indep;
-    hid_t dxplid_coll_nb;
-    hid_t dxplid_indep_nb;
+class e3sm_io_driver_hdf5_log : public e3sm_io_driver_hdf5 {
     hid_t log_vlid;
-
-    hsize_t one[H5S_MAX_RANK];
-
+        
     // Config
-    bool use_logvol       = false;
     bool use_logvol_varn  = false;
-    bool use_dwrite_multi = false;
     bool merge_varn       = false;
 
    public:
     e3sm_io_driver_hdf5_log (e3sm_io_config *cfg);
     ~e3sm_io_driver_hdf5_log ();
-    int create (std::string path, MPI_Comm comm, MPI_Info info, int *fid);
-    int open (std::string path, MPI_Comm comm, MPI_Info info, int *fid);
-    int close (int fid);
-    int inq_file_info (int fid, MPI_Info *info);
-    int inq_file_size (std::string path, MPI_Offset *size);
-    int inq_put_size (MPI_Offset *size);
-    int inq_get_size (MPI_Offset *size);
-    int inq_malloc_size (MPI_Offset *size);
-    int inq_malloc_max_size (MPI_Offset *size);
-    int inq_rec_size (int fid, MPI_Offset *size);
-    int def_var (int fid, std::string name, nc_type xtype, int ndim, int *dimids, int *did);
-    int def_local_var (
-        int fid, std::string name, nc_type xtype, int ndim, MPI_Offset *dsize, int *did);
-    int inq_var (int fid, std::string name, int *did);
-    int inq_var_name(int ncid, int varid, char *name);
-    int inq_var_off (int fid, int vid, MPI_Offset *off);
-    int def_dim (int fid, std::string name, MPI_Offset size, int *dimid);
-    int inq_dim (int fid, std::string name, int *dimid);
-    int inq_dimlen (int fid, int dimid, MPI_Offset *size);
-    int enddef (int fid);
-    int redef (int fid);
-    int wait (int fid);
-    int put_att (int fid, int vid, std::string name, nc_type xtype, MPI_Offset size, const void *buf);
-    int get_att (int fid, int vid, std::string name, void *buf);
-    int put_varl (int fid, int vid, MPI_Datatype itype, void *buf, e3sm_io_op_mode mode);
+    int create (std::string path, MPI_Comm comm, MPI_Info info, int *fid) override;
+    int open (std::string path, MPI_Comm comm, MPI_Info info, int *fid) override;
+   
     int put_vara (int fid,
                   int vid,
                   MPI_Datatype itype,
                   MPI_Offset *start,
                   MPI_Offset *count,
                   void *buf,
-                  e3sm_io_op_mode mode);
+                  e3sm_io_op_mode mode) override;
     int put_vars (int fid,
                   int vid,
                   MPI_Datatype itype,
@@ -92,7 +46,7 @@ class e3sm_io_driver_hdf5_log : public e3sm_io_driver {
                   MPI_Offset *count,
                   MPI_Offset *stride,
                   void *buf,
-                  e3sm_io_op_mode mode);
+                  e3sm_io_op_mode mode) override;
     int put_varn (int fid,
                   int vid,
                   MPI_Datatype itype,
@@ -100,14 +54,14 @@ class e3sm_io_driver_hdf5_log : public e3sm_io_driver {
                   MPI_Offset **starts,
                   MPI_Offset **counts,
                   void *buf,
-                  e3sm_io_op_mode mode);
+                  e3sm_io_op_mode mode) override;
     int get_vara (int fid,
                   int vid,
                   MPI_Datatype itype,
                   MPI_Offset *start,
                   MPI_Offset *count,
                   void *buf,
-                  e3sm_io_op_mode mode);
+                  e3sm_io_op_mode mode) override;
     int get_vars (int fid,
                   int vid,
                   MPI_Datatype itype,
@@ -115,7 +69,7 @@ class e3sm_io_driver_hdf5_log : public e3sm_io_driver {
                   MPI_Offset *count,
                   MPI_Offset *stride,
                   void *buf,
-                  e3sm_io_op_mode mode);
+                  e3sm_io_op_mode mode) override;
     int get_varn (int fid,
                   int vid,
                   MPI_Datatype itype,
@@ -123,7 +77,7 @@ class e3sm_io_driver_hdf5_log : public e3sm_io_driver {
                   MPI_Offset **starts,
                   MPI_Offset **counts,
                   void *buf,
-                  e3sm_io_op_mode mode);
+                  e3sm_io_op_mode mode) override;
 };
 
 
