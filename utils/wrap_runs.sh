06b5739f42a15f2dec82d21b305931e9ba7a78df
wkliao <wkliao@users.noreply.github.com>
2022-03-24 16:20:27 -0500

    use BUILT_SOURCES to create list_f_case.txt

--- utils/wrap_runs.sh
+++ utils/wrap_runs.sh
@@ -10,10 +10,9 @@ set -e
 unset HDF5_VOL_CONNECTOR
 unset HDF5_PLUGIN_PATH
 
-rm -f list_f_case.txt
-ls -1 $top_srcdir/datasets/piodecomp16tasks*.dat > list_f_case.txt
-echo "==== cat list_f_case.txt ===="
-cat list_f_case.txt
+LIST_FILE=${builddir}/list_f_case.txt
+echo "==== cat $LIST_FILE ===="
+cat $LIST_FILE
 echo ""
 
 NC_FILE=$top_srcdir/datasets/f_case_866x72_16p.nc
@@ -23,28 +22,28 @@ export LD_LIBRARY_PATH=${ALL_LIBRARY_PATH}:${LD_LIBRARY_PATH}
 
 if test "$1" = ./dat2nc ; then
    rm -f $1.nc
-   CMD="$1 -i list_f_case.txt -o $1.nc"
+   CMD="$1 -i $LIST_FILE -o $1.nc"
    echo $CMD
    $CMD
    rm -f $1.nc
 elif test "$1" = ./dat2decomp ; then
    if test "x${ENABLE_HDF5}" = x1 ; then
       rm -f $1.h5
-      CMD="$1 -a hdf5 -i list_f_case.txt -o $1.h5"
+      CMD="$1 -a hdf5 -i $LIST_FILE -o $1.h5"
       echo $CMD
       $CMD
       rm -f $1.h5
    fi
    if test "x${ENABLE_NETCDF4}" = x1 ; then
       rm -f $1.nc4
-      CMD="$1 -a netcdf4 -i list_f_case.txt -o $1.nc4"
+      CMD="$1 -a netcdf4 -i $LIST_FILE -o $1.nc4"
       echo $CMD
       $CMD
       rm -f $1.nc4
    fi
    if test "x${ENABLE_ADIOS2}" = x1 ; then
       rm -f $1.nc4
-      CMD="$1 -a bp -i list_f_case.txt -o $1.bp"
+      CMD="$1 -a bp -i $LIST_FILE -o $1.bp"
       echo $CMD
       $CMD
       rm -rf $1.bp.dir
@@ -77,5 +76,4 @@ elif test "$1" = ./datstat ; then
    $CMD
 fi
 
-rm -f list_f_case.txt
 
