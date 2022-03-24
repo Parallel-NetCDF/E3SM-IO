44aa2cd200591c30bfe14c1fc215ea4ef7e9be15
wkliao <wkliao@users.noreply.github.com>
2022-03-24 01:48:03 -0500

    Use revise command-line option -a cdf5 or bp

--- test.sh
+++ test.sh
@@ -10,11 +10,13 @@ set -e
 DECOMREPLAY="utils/decomreplay"
 DECOMS=()
 
+VERBOSE=0
 EXEC="src/e3sm_io"
 if test "x$#" = x0 ; then
-    RUN=""
+   RUN=""
+   VERBOSE=1
 else
-    RUN="${TESTMPIRUN} -n $1"
+   RUN="${TESTMPIRUN} -n $1"
 fi
 
 CONFIGS=("f_case_866x72_16p" "g_case_cmpaso_16p" "i_case_f19_g16_16p")
@@ -42,19 +44,22 @@ fi
 if test "x${ENABLE_ADIOS2}" = x1 ; then
    APIS+=("adios blob")
    export LD_LIBRARY_PATH=${ADIOS2_LIB_PATH}:${LD_LIBRARY_PATH}
-   DECOMS+=("adios bp")
+   DECOMS+=("bp bp")
 fi
 
 if test "x${ENABLE_NETCDF4}" = x1 ; then
-   echo "====================================================================="
-   echo "Warning: skip NetCDF-4 tests due to a bug in NetCDF-C 4.8.1 and prior"
-   echo "         See https://github.com/Unidata/netcdf-c/issues/2251"
-   echo "====================================================================="
-#   APIS+=("netcdf4 canonical")
-#   export LD_LIBRARY_PATH=${HDF5_LIB_PATH}:${NETCDF4_LIB_PATH}:${LD_LIBRARY_PATH}
-#   if test "x${ENABLE_LOGVOL}" = x1 ; then
-#      APIS+=("netcdf4 log")
-#   fi
+   if test "x$#" = x0 ; then :; else
+      echo "==================================================================="
+      echo "Warning: skip NetCDF-4 parallel tests due to a bug in NetCDF-C"
+      echo "         version 4.8.1 and prior. See bug issue"
+      echo "         https://github.com/Unidata/netcdf-c/issues/2251"
+      echo "==================================================================="
+   fi
+   APIS+=("netcdf4 canonical")
+   export LD_LIBRARY_PATH=${HDF5_LIB_PATH}:${NETCDF4_LIB_PATH}:${LD_LIBRARY_PATH}
+   if test "x${ENABLE_LOGVOL}" = x1 ; then
+      APIS+=("netcdf4 log")
+   fi
    DECOMS+=("netcdf4 nc4")
 fi
 
@@ -66,13 +71,24 @@ unset HDF5_PLUGIN_PATH
 unset HDF5_VOL_CONNECTOR
 for DECOM in "${DECOMS[@]}" ; do
     dc=($DECOM)
-    DECOM_FORMAT=${dc[0]}
-    OUT_FILE_EXT=${dc[1]}
+    api=${dc[0]}
     for CONFIG in "${CONFIGS[@]}" ; do
-        if test "x${DECOM_FORMAT}" != xpnetcdf ; then
-           ${DECOMREPLAY} -a ${DECOM_FORMAT} -i datasets/${CONFIG}.nc -o datasets/${CONFIG}.${OUT_FILE_EXT}
-           if test "x${DECOM_FORMAT}" == xadios ; then
-              cp datasets/${CONFIG}.${OUT_FILE_EXT}.dir/${CONFIG}.${OUT_FILE_EXT}.0 datasets/${CONFIG}.${OUT_FILE_EXT}
+        IN_FILE=$srcdir/datasets/${CONFIG}.nc
+        OUT_FILE=datasets/${CONFIG}.${dc[1]}
+        if test -e $OUT_FILE ; then
+           if test $VERBOSE = 1 ; then echo "$OUT_FILE already exist"; fi
+        else
+           if test $VERBOSE = 1 ; then echo "Convert $IN_FILE into $OUT_FILE"; fi
+           CMD="${DECOMREPLAY} -a ${api} -i ${IN_FILE} -o ${OUT_FILE}"
+           if test $VERBOSE = 1 ; then echo "CMD=$CMD"; fi
+           $CMD
+           if test "x${api}" = xbp ; then
+              CMD="mv -f ${OUT_FILE}.dir/${CONFIG}.${dc[1]}.0 ${OUT_FILE}"
+              if test $VERBOSE = 1 ; then echo "CMD=$CMD"; fi
+              $CMD
+              CMD="rm -rf ${OUT_FILE}.dir"
+              if test $VERBOSE = 1 ; then echo "CMD=$CMD"; fi
+              $CMD
            fi
         fi
     done
@@ -81,36 +97,39 @@ done
 for API in "${APIS[@]}" ; do
     ap=($API)
 
-     if test "x${ap[0]}" = xnetcdf4 && test "x${ap[1]}" = xlog ; then
-        # This option requires the two VOL environment variables to be set.
-        export HDF5_PLUGIN_PATH="$LOGVOL_LIB_PATH"
-        export HDF5_VOL_CONNECTOR="LOG under_vol=0;under_info={}"
-        # Decomposition file must be read with native VOL, use nc file
-        DECOM_EXT="nc"
-     else
-        # unset the two VOL environment variables, in case they have been set
-        unset HDF5_PLUGIN_PATH
-        unset HDF5_VOL_CONNECTOR
-     fi
+    # unset the two VOL environment variables, in case they have been set
+    unset HDF5_PLUGIN_PATH
+    unset HDF5_VOL_CONNECTOR
 
     for CONFIG in "${CONFIGS[@]}" ; do
-        if test "x${ap[0]}" = xnetcdf4 ; then
-           OUT_FILE_EXT="nc4"
-           DECOM_EXT="nc4"
-        elif test "x${ap[0]}" = xpnetcdf ; then
-           OUT_FILE_EXT="nc"
-           DECOM_EXT="nc"
+        IN_FILE=datasets/${CONFIG}
+        OUT_FILE="${OUT_PATH}/${ap[0]}_${ap[1]}_${CONFIG}"
+        if test "x${ap[0]}" = xpnetcdf ; then
+           IN_FILE="${srcdir}/${IN_FILE}.nc"
+           OUT_FILE+=".nc"
+        elif test "x${ap[0]}" = xnetcdf4 ; then
+           if test "x$#" = x0 ; then :; else continue; fi
+           OUT_FILE+=".nc4"
+           if test "x${ap[1]}" = xlog ; then
+              # This option requires the two VOL environment variables to be set.
+              export HDF5_PLUGIN_PATH="$LOGVOL_LIB_PATH"
+              export HDF5_VOL_CONNECTOR="LOG under_vol=0;under_info={}"
+              # Decomposition file must be read with native VOL, use nc file
+              IN_FILE="${srcdir}/${IN_FILE}.nc"
+           else
+              IN_FILE+=".nc4"
+           fi
         elif test "x${ap[0]}" = xhdf5 || test "x${ap[0]}" = xhdf5_log ; then
-           OUT_FILE_EXT="h5"
-           DECOM_EXT="h5"
+           IN_FILE+=".h5"
+           OUT_FILE+=".h5"
         elif test "x${ap[0]}" = xadios ; then
-           OUT_FILE_EXT="bp"
-           DECOM_EXT="bp"
+           IN_FILE+=".bp"
+           OUT_FILE+=".bp"
         fi
-        OUT_FILE="${OUT_PATH}/${ap[0]}_${ap[1]}_${CONFIG}.${OUT_FILE_EXT}"
-        
-        echo "${RUN} ${EXEC} -k -a ${ap[0]} -f -1 -r 2 -x ${ap[1]} -y 2 -o ${OUT_FILE} ${srcdir}/datasets/${CONFIG}.${DECOM_EXT}"
-        ${RUN} ${EXEC} -k -a ${ap[0]} -f -1 -r 2 -x ${ap[1]} -y 2 -o ${OUT_FILE} ${srcdir}/datasets/${CONFIG}.${DECOM_EXT}
+
+        CMD="${RUN} ${EXEC} -k -a ${ap[0]} -f -1 -r 2 -x ${ap[1]} -y 2 -o ${OUT_FILE} ${IN_FILE}"
+        if test $VERBOSE = 1 ; then echo "${CMD}"; fi
+        ${CMD}
     done
 done
 
