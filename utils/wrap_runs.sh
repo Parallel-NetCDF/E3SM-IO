e2bcd2f39416e3a0fa2a7b9a7b855cd23381b7a3
wkliao <wkliao@users.noreply.github.com>
2022-03-24 15:48:15 -0500

    set LD_LIBRARY_PATH

--- utils/wrap_runs.sh
+++ utils/wrap_runs.sh
@@ -18,29 +18,36 @@ echo ""
 
 NC_FILE=$top_srcdir/datasets/f_case_866x72_16p.nc
 
+ALL_LIBRARY_PATH=`echo $ALL_LIBRARY_PATH | ${SED} 's/ //g'`
+export LD_LIBRARY_PATH=${ALL_LIBRARY_PATH}:${LD_LIBRARY_PATH}
+
 if test "$1" = ./dat2nc ; then
    rm -f $1.nc
    CMD="$1 -i list_f_case.txt -o $1.nc"
    echo $CMD
    $CMD
+   rm -f $1.nc
 elif test "$1" = ./dat2decomp ; then
    if test "x${ENABLE_HDF5}" = x1 ; then
       rm -f $1.h5
       CMD="$1 -a hdf5 -i list_f_case.txt -o $1.h5"
       echo $CMD
       $CMD
+      rm -f $1.h5
    fi
    if test "x${ENABLE_NETCDF4}" = x1 ; then
       rm -f $1.nc4
       CMD="$1 -a netcdf4 -i list_f_case.txt -o $1.nc4"
       echo $CMD
       $CMD
+      rm -f $1.nc4
    fi
    if test "x${ENABLE_ADIOS2}" = x1 ; then
       rm -f $1.nc4
       CMD="$1 -a bp -i list_f_case.txt -o $1.bp"
       echo $CMD
       $CMD
+      rm -rf $1.bp.dir
    fi
 elif test "$1" = ./decomp_copy ; then
    if test "x${ENABLE_PNC}" = x1 && test "x${ENABLE_HDF5}" = x1 ; then
@@ -48,18 +55,21 @@ elif test "$1" = ./decomp_copy ; then
       CMD="$1 -a hdf5 -i $NC_FILE -o $1.h5"
       echo $CMD
       $CMD
+      rm -f $1.h5
    fi
    if test "x${ENABLE_PNC}" = x1 && test "x${ENABLE_NETCDF4}" = x1 ; then
       rm -f $1.nc4
       CMD="$1 -a netcdf4 -i $NC_FILE -o $1.nc4"
       echo $CMD
       $CMD
+      rm -f $1.nc4
    fi
    if test "x${ENABLE_PNC}" = x1 && test "x${ENABLE_ADIOS2}" = x1 ; then
       rm -f $1.nc4
       CMD="$1 -a bp -i $NC_FILE -o $1.bp"
       echo $CMD
       $CMD
+      rm -rf $1.bp.dir
    fi
 elif test "$1" = ./datstat ; then
    CMD="$1 -d $top_srcdir/datasets/piodecomp16tasks16io02dims_ioid_548.dat"
@@ -67,5 +77,5 @@ elif test "$1" = ./datstat ; then
    $CMD
 fi
 
-rm -rf list_f_case.txt $1.nc $1.h5 $1.nc4 $1.bp.dir
+rm -f list_f_case.txt
 
