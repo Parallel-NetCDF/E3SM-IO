b0e151602107e092a8caf666eb25692ebd824011
wkliao <wkliao@users.noreply.github.com>
2022-07-17 12:23:14 -0500

    fix setting decom_cfg.num_iotasks to nprocs

--- src/read_decomp.cpp
+++ src/read_decomp.cpp
@@ -128,7 +128,7 @@ int read_decomp (e3sm_io_config *cfg, e3sm_io_decom *decom) {
     // Set up config for read driver
     decom_cfg.io_comm        = MPI_COMM_WORLD;
     decom_cfg.info           = MPI_INFO_NULL;
-    decom_cfg.num_iotasks    = decom_cfg.np;
+    decom_cfg.num_iotasks    = nprocs;
     decom_cfg.num_group      = 1;
     decom_cfg.out_path[0]    = '\0';
     decom_cfg.in_path[0]     = '\0';
