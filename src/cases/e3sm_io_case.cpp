989fb2240014a9c73259e82bf23d6456472c5e44
wkliao <wkliao@users.noreply.github.com>
2022-10-27 12:38:47 -0500

    fix h0 and h1 file names for BP5 files

--- src/cases/e3sm_io_case.cpp
+++ src/cases/e3sm_io_case.cpp
@@ -81,7 +81,7 @@ int e3sm_io_case::wr_test(e3sm_io_config &cfg,
             cmeta->ffreq = cmeta->nrecs;
 
         /* construct file name */
-        if (ext == NULL || (strcmp(ext, ".nc") && strcmp(ext, ".h5") && strcmp(ext, ".nc4")))
+        if (ext == NULL || (strcmp(ext, ".nc") && strcmp(ext, ".h5") && strcmp(ext, ".nc4") && strcmp(ext, ".bp")))
             sprintf(cmeta->outfile, "%s_h0", cfg.out_path);
         else { /* add "_h0" before file extension */
             strcpy(cmeta->outfile, cfg.out_path);
@@ -118,7 +118,7 @@ int e3sm_io_case::wr_test(e3sm_io_config &cfg,
             cmeta->ffreq = cmeta->nrecs;
 
         /* construct file name */
-        if (ext == NULL || (strcmp(ext, ".nc") && strcmp(ext, ".h5") && strcmp(ext, ".nc4")))
+        if (ext == NULL || (strcmp(ext, ".nc") && strcmp(ext, ".h5") && strcmp(ext, ".nc4") && strcmp(ext, ".bp")))
             sprintf(cmeta->outfile, "%s_h1", cfg.out_path);
         else { /* add "_h1" before file extension */
             strcpy(cmeta->outfile, cfg.out_path);
